99bc29a69f24ac857422e863779ff8798d3f41d0
190996Rohitkumar <74916042+190996Rohitkumar@users.noreply.github.com>
2024-03-29 13:25:15 +0530

    exception handled

--- db.php
+++ db.php
@@ -3,10 +3,14 @@ $dbuser="root";
 $dbname="phpcrud";
 $hostname="localhost";
 $dbpass="R0hit@51471";
+try{
+    $conn=new mysqli($hostname,$dbuser,$dbpass,$dbname);
+}catch(Exception $e){
+    echo $e->getMessage();
+}
 
-$conn=new mysqli($hostname,$dbuser,$dbpass,$dbname);
-echo "<pre>";
-print_r($conn);
+echo "db";
+
 
 
 
