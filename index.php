f0b304d33c8084966844134476b57c4b2224486e
190996Rohitkumar <74916042+190996Rohitkumar@users.noreply.github.com>
2024-03-29 13:30:15 +0530

    print added

--- index.php
+++ index.php
@@ -16,13 +16,13 @@ $dbuser="root";
 $dbname="phpcrud";
 $hostname="localhost";
 $dbpass="R0hit@51471";
-try{
-    $conn=new mysqli($hostname,$dbuser,$dbpass,$dbname);
-}catch(Exception $e){
-    echo $e->getMessage();
-}
+// try{
+//     $conn=new mysqli($hostname,$dbuser,$dbpass,$dbname);
+// }catch(Exception $e){
+//     echo $e->getMessage();
+// }
 
-echo "db";
+echo $dbuser;
 
 
 
