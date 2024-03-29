816e0cc1c86d10e7802c5f31938911065fc8f3b3
190996Rohitkumar <74916042+190996Rohitkumar@users.noreply.github.com>
2024-03-29 14:15:56 +0530

    db check

--- db.php
+++ db.php
@@ -5,6 +5,7 @@ $hostname="localhost";
 $dbpass="R0hit@51471";
 try{
     $conn=new mysqli($hostname,$dbuser,$dbpass,$dbname);
+    print_r($conn);
 }catch(Exception $e){
     echo $e->getMessage();
 }
